829d8691c103fb64f3b87d5c493549e718628530
ZeynepMahinur <122391696+ZeynepMahinur@users.noreply.github.com>
2025-06-21 17:54:14 +0300

    Add files via upload

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -1,38 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zarikan <zarikan@student.42istanbul.com.t  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/21 17:53:27 by zarikan           #+#    #+#             */
+/*   Updated: 2025/06/21 17:53:28 by zarikan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-int    ft_printf(const char *f, ...)
+static int	formats(char c, va_list args)
 {
-    va_list args;
-    va_start(args, f);
-    int i;
-    int c;
+	if (c == '%')
+		c += ft_percent();
+	else if (c == 'd' || c == 'i')
+		c += ft_putnbr(va_arg(args, int));
+	else if (c == 'X')
+		c += ft_putnbr_base(va_arg(args, int), "0123456789ABCDEF");
+	else if (c == 'p')
+		c += ft_putnbr_base(va_arg(args, int), "0x");
+	else if (c == 'u')
+		c += ft_putnbr_base(va_arg(args, int), "0123456789");
+	else if (c == 'x')
+		c += ft_putnbr_base(va_arg(args, int), "0123456789abcdef");
+	else if (c == 'c')
+		c += ft_putchar(va_arg(args, int));
+	else if (c == 's')
+		c += ft_putstr(va_arg(args, char *));
+	return (0);
+}
 
-    i = 0;
-    c = 0;
-    while (f[i])
-    {
-        if (f[i] == '%' && f[i + 1])
-        {
-            i++;
-            if (f[i] == '%')
-                c += ft_percent();
-            else if (f[i] == 'd' || f[i] == 'i')
-                c += ft_putnbr(va_arg(args, int));
-            else if (f[i] == 'X')
-                c += ft_putnbr_base(va_arg(args, int), "0123456789ABCDEF");
-            else if (f[i] == 'p')
-                c += ft_putnbr_base(va_arg(args, int), "0x");
-            else if (f[i] == 'u')
-                c += ft_putnbr_base(va_arg(args, int), "0123456789");
-            else if (f[i] == 'x')
-                c += ft_putnbr_base(va_arg(args, int), "0123456789abcdef");
-            else if (f[i] == 'c')
-                c += ft_putchar(va_arg(args, int));
-            else if (f[i] == 's')
-                c += ft_putstr(va_arg(args, char *));
-        }
-        i++;
-    }
-    va_end(args);
-    return (c);
+int	ft_printf(const char *f, ...)
+{
+	int		i;
+	int		c;
+	va_list	args;
+
+	va_start (args, f);
+	i = 0;
+	c = 0;
+	while (f[i])
+	{
+		if (f[i] == '%' && f[i + 1])
+		{
+			i++;
+			c += formats(f[i], args);
+		}
+		else
+			ft_putchar(f[i]);
+		i++;
+	}
+	va_end(args);
+	return (c);
 }
